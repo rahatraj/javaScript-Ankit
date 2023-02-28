9af992a9206e43921b7e9f114ca46ec9cf5bd180
Rahat Reza <rahatreza3199@gmail.com>
2023-02-28 13:45:16 +0530

    get sum by using reduce function

--- sumOfOdd.js
+++ sumOfOdd.js
@@ -1,10 +1,21 @@
-function sum_of_odd(arr){
-    var sum =0;
-    for(var i=0;i<arr.length;i++){
-        if(arr[i]%2!=0){
-            sum +=arr[i];
+// function sum_of_odd(arr){
+//     var sum =0;
+//     for(var i=0;i<arr.length;i++){
+//         if(arr[i]%2!=0){
+//             sum +=arr[i];
+//         }
+//     }
+//     return sum;
+// }
+// console.log(sum_of_odd([2,3,4,5,6,7,8,9]))
+
+function sumOfOdd(arr){
+    debugger;
+    return arr.reduce(function(total,value,initialValue){
+        if(value%2 !== 0){
+        return total +=value
         }
-    }
-    return sum;
+        return total;
+    });
 }
-console.log(sum_of_odd([2,3,4,5,6,7,8,9]))
+console.log(sumOfOdd([2,3,4,5,6,7,8,9]))
